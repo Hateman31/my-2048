e7b36473a943de243a57d97ea628d2fe9709a696
hateman31 <metilogrune31@gmail.com>
2020-05-26 12:30:05 +0300

    checking end of game

--- src/my_2048/core.cljs
+++ src/my_2048/core.cljs
@@ -13,13 +13,38 @@
 (def grid
   (u/get-grid 80 4 3))
 
-(let [
-  game-matrix 
-    [[2 4 2 4]
-    [2 4 2 4]
-    [2 4 2 4]
-    [2 4 2 4]]
-  game-state (g/matrix-to-vector game-matrix)
-  game-field (map list grid game-state)]
-  (u/draw-field background game-field))
+(def game-state
+  (atom [
+    [4 2 2 4]
+    [16 8 4 2]
+    [32 16 128 256]
+    [2 4 2 4]]))
 
+(defn render-game [game-state]
+  (let [
+      game-field (map list grid (g/matrix-to-vector game-state))] 
+    (u/clear-canvas game)
+    (u/draw-field background game-field)))
+
+(.addEventListener js/document "keydown" 
+  (fn [event] 
+    (let [direction (u/arrow-direction event.key)]
+      (if direction
+        (let [shift #(g/update-grid %1 direction)]
+          ; (println @game-state)
+          (swap! game-state shift))))))
+
+(add-watch game-state :updating
+  #(render-game %4))
+
+(add-watch game-state :game-ending
+  #((let [game-state %4]
+      (cond 
+        (g/win? game-state)
+        (do (u/clear-canvas game)
+         (js/alert "You won!"))
+        (g/lose? game-state)
+        (do (u/clear-canvas game)
+         (js/alert "You lost!"))))))
+
+(render-game @game-state)
