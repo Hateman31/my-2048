c2a86152aec0d30a1d43ffd4bcab1b4f0492cbda
hateman31 <metilogrune31@gmail.com>
2020-07-01 12:06:46 +0300

    add tiles colortable

--- src/my_2048/utils.cljs
+++ src/my_2048/utils.cljs
@@ -6,6 +6,20 @@
       "ArrowLeft" :left
       "ArrowRight" :right})
 
+(def tile-color {
+  0 "#776e65",
+  2 "#eee4da",
+  4 "#ede0c8",
+  8 "#f2b179",
+  16 "#f59563",
+  32 "#f67c5f",
+  64 "#f65e3b",
+  128 "#edcf72",
+  256 "#edcc61",
+  512 "#edcc61",
+  1024 "#edc53f",
+  2048 "#edc22e"})
+
 (defn draw-square [point ctx]
   (let [[m n] point]
     (.fillRect ctx m n 80 80)))
